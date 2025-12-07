cefbd2a33489c756ed1d44aecbcd065f2763e5ce
naveenshan-hub <officials.vee@gmail.com>
2025-12-07 14:17:02 +0000

    Replace entire repository with local version

--- analysis_scripts/aa_conversion.py
+++ analysis_scripts/aa_conversion.py
@@ -1,45 +1,50 @@
-
-from Bio.Seq import Seq
-from Bio.SeqUtils import gc_fraction
-from Bio.SeqRecord import SeqRecord
-from Bio import SeqIO
-
-fasta_file = r"C:\Users\offic\bash_practice\sequences\samples\for_translation.fasta"
+# import required objects from biopython
+from Bio.Seq import Seq  # takes care of DNA sequence objects
+from Bio.SeqRecord import SeqRecord  # stores ID and description
+from Bio import SeqIO  # reads and writes fasta
+# input the required fasta file
+fasta_file = r"C:\Users\offic\BIF\Bioinformatics_assignment\results\for_translation.fasta"
+# reads all the sequences from input and stores it in seqeecord objects
 sequences = list(SeqIO.parse(fasta_file, "fasta"))
 
+# just to make sure of all the reference and sample sequences are taken in
 print("number of sequences:", len(sequences))
 
-
-aa_sequences = []
-
+aa_sequences = []  # an empty list to store protein sequences after translation
+# looping through each sequences
 for record in sequences:
-    dna_seq = record.seq
-    best_orf = ""
+    dna_seq = record.seq  # extract the sequneces
+    best_orf = ""  # variables to keep best orf record
     best_frame = None
-
+# check translation in all three forward reading frames
     for frame in range(3):
-        protein = dna_seq[frame:].translate(to_stop=False)
+        protein = dna_seq[frame:].translate(
+            to_stop=False)  # trasnlate from this frame
+        # split translation into fragments seperated by stop codons
         fragments = str(protein).split("*")
+        # select longest fragment as longest orf in that frame
         longest_in_frame = max(fragments, key=len)
-
+# keep the best longest orf among all frames
         if len(longest_in_frame) > len(best_orf):
             best_orf = longest_in_frame
             best_frame = frame
-
+# convert selected protein sequence into seq object
     best_protein = Seq(best_orf)
-
+# create clean sequence ID
     des_words = record.description.split()
+    # get the genus and species name
     scientific_name = "_".join(des_words[1:3])
     new_id = f"{record.id}_{scientific_name}"
-
+# create a new seqrecord for translated protein
     aa_record = SeqRecord(
-        best_protein,
-        id=new_id,
-        description=""
+        best_protein,  # amino acid sequence
+        id=new_id,  # new ID
+        description=""  # remove unwanted descriptions
     )
+    # add the output sequence to the output list
     aa_sequences.append(aa_record)
-
-
-output_file = r"C:\Users\offic\bash_practice\sequences\samples\translated.fasta"
+# save all translated proteins into a fasta file
+output_file = r"C:\Users\offic\BIF\Bioinformatics_assignment\results\translated.fasta"
 SeqIO.write(aa_sequences, output_file, "fasta")
+# this message pops after the whole process is complete
 print("Translated proteins saved to:", output_file)
--- analysis_scripts/tester.sh
+++ analysis_scripts/tester.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+# change to raw_data directory to get the sample files
+cd raw_data || exit
+# loop through the files that matches the given pattern
 for file in sample*_part1.FASTQ;do
-  base=${file%_part1.FASTQ}
-  cat ${base}_part*.FASTQ |
-  sed 's/@/\n@/g'| awk 'NF{ if (/^@/) { if ($0==last_header) next; last_header=$0} print}'> ${base}_combined.FASTQ
+  base=${file%_part1.FASTQ} # get the base name only by removing _part1.FASTQ part
+  cat ${base}_part*.FASTQ | # concatenate all split samples respect to their parts
+  #  add new lines after each @ and remove repeated headers and get combined FASTQ files
+  sed 's/@/\n@/g'| awk 'NF{ if (/^@/) { if ($0==last_header) next; last_header=$0}
+ print}'> ${base}_combined.FASTQ
+# converting FASTQ -> fasta 
+# convert @ to > and print 1st and 2nd lines  i.e. the header & Sequences 
   awk 'NR%4==1 {sub(/^@/,">"); print} NR%4==2 {print}' "${base}_combined.FASTQ"> "${base}.fasta"
-rm -f *_combined.FASTQ
+rm -f *_combined.FASTQ  # removed the combined FASTQ files which is not required
 done
-rm -f *clean.fasta
+rm -f *clean.fasta # remove any existing files in this name 
+# processing each fasta files
 for file in sample*.fasta;do
-  base=${file%.fasta}
-  awk -v base=$base '/^>/ {next} {gsub (/ /,""); seq = seq $0} END {print ">" base; print seq}' "$file" >> "${base}clean.fasta"
+  base=${file%.fasta} 
+# skip the headers, remove spaces in between the sequences and concatenate the result sequences and print it 
+  awk -v base="$base" '/^>/ {next} {gsub (/ /,""); seq = seq $0} END {print ">" base; print seq}' "$file" >> "${base}clean.fasta"
 done
-echo "fasta file is ready"
+cd "C:\Users\offic\BIF\Bioinformatics_assignment" # moving into main directory
+mkdir -p cleaned_data # create a new folder for saving the clean fasta sequences
+mv raw_data/*clean.fasta cleaned_data/  # move fasta files from raw_data to cleaned_data folder
+echo "fasta files are saved in cleaned_data folder" 
+
+
+
